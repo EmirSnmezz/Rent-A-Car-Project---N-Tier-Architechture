e41c3c8429bb6c2c9c68253035d79e2d2790983a
Emir <emircan_snmez@outlook.com>
2025-06-22 03:22:51 +0300

    Core layer created. General classes and interfaces moved to the layer

---
+++ Core/DataAccess/Abstract/Base/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Entities.Abstract;
+
+namespace Core.DataAccess.Abstract.Base
+{
+    public interface IEntityRepository<TEntity> where TEntity : class, IEntity
+    {
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+        TEntity GetById(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ Core/DataAccess/EntityFramework/EntityRepositoryBase.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq.Expressions;
+using Core.DataAccess.Abstract.Base;
+using Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Core.DataAccess.EntityFramework;
+
+public class EntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity> 
+    where TEntity : class, IEntity, new()
+    where TContext : DbContext, new()
+{
+
+    public DbContext _context;
+    public EntityRepositoryBase(DbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(TEntity entity)
+    {
+        EntityEntry addedEntity = _context.Set<TEntity>().Entry(entity);
+        EntityState state = EntityState.Added;
+        _context.SaveChanges();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        EntityEntry deletedEntity = _context.Set<TEntity>().Entry(entity);
+        EntityState state = EntityState.Deleted;
+        _context.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        EntityEntry updatedEntity = _context.Set<TEntity>().Entry(entity);
+        EntityState state = EntityState.Modified;
+        _context.SaveChanges();
+    }
+
+    public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+    {
+        return filter == null 
+            ? _context.Set<TEntity>().ToList() 
+            : _context.Set<TEntity>().Where(filter).ToList();
+    }
+
+    public TEntity GetById(Expression<Func<TEntity, bool>> filter)
+    {
+        TEntity entity =  _context.Set<TEntity>().FirstOrDefault(filter);
+
+        if(entity is not null)
+        {
+            return entity;
+        }
+
+        return null;
+    }
+}
