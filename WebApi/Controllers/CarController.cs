7470ee5786aa8b015d5cf73c11624c722ab123b2
Emir <emircan_snmez@outlook.com>
2025-06-28 01:14:45 +0300

    Endpoints written for Car Entity

--- WebApi/Controllers/CarController.cs
+++ WebApi/Controllers/CarController.cs
@@ -38,5 +38,72 @@ namespace WebApi.Controllers
             return BadRequest(result.Message);
 
         }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Car car)
+        {
+            var result = _carService.Delete(car);
+
+            if(result.IsSuccess)
+                return Ok(result.Message);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Car car)
+        {
+            var result = _carService.Update(car);
+
+            if(result.IsSuccess)
+                return Ok(result.Message);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("getbyid")]
+        public IActionResult GetById([FromBody] int id)
+        {
+            var result = _carService.GetById(id);
+
+            if(result.IsSuccess)
+                return Ok(result.Data);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost ("getbybrandid")]
+        public IActionResult GetByBrandId(int brandId)
+        {
+            var result = _carService.GetByBrandId(brandId);
+
+            if(result.IsSuccess)
+                return Ok(result.Data);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getbyprice")]
+        public IActionResult GetByPrice(int minValue, int maxValue)
+        {
+            var result = _carService.GetByPrice(minValue, maxValue);
+
+            if(result.IsSuccess)
+                return Ok(result.Data);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getcardetail")]
+        public IActionResult GetAllCarDetails()
+        {
+            var result = _carService.GetCarDetails();
+
+            if(result.IsSuccess)
+                return Ok(result.Data);
+
+            return BadRequest(result.Message);
+        }
+
     }
 }
