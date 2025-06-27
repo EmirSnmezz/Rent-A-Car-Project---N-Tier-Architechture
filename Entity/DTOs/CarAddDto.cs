13d9c4fd340641992eef76a992418ca4d86a0bd6
Emir <emircan_snmez@outlook.com>
2025-06-27 04:54:43 +0300

    Dto Object Created For Car Entity, Properties fixed for Entity Framework Of Car Entity

--- Entity/DTOs/CarAddDto.cs
+++ Entity/DTOs/CarAddDto.cs
@@ -1,28 +1,22 @@
-﻿using Entities.Abstract;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Entities.Concrete
+namespace Entities.DTOs
 {
-    public class Car : IEntity
+    public class CarAddDto
     {
-        public int Id { get; set; }
-
         public int BrandId { get; set; }
-        public Brand Brand { get; set; } = null!;
-
         public int ModelId { get; set; }
-        public Model Model { get; set; } = null!;
-
         public int ColorId { get; set; }
-        public Color Color { get; set; } = null!;
-
         public int CompanyId { get; set; }
-        public Company Company { get; set; } = null!;
 
         public bool IsRented { get; set; }
         public int ModelYear { get; set; }
         public string GearBoxOption { get; set; }
         public int CarMileage { get; set; }
-
         public int Price { get; set; }
     }
 }
