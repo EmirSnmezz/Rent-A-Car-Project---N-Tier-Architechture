0bbf44e041971a29e61a343de06de06b73dea886
Emir <emircan_snmez@outlook.com>
2025-06-27 04:53:42 +0300

    Car Controller Fixed For Generic Repository Class And Dal Class

--- WebApi/Controllers/CarController.cs
+++ WebApi/Controllers/CarController.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Controllers.Base;
 
@@ -16,9 +18,25 @@ namespace WebApi.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
-            var result = _carService.GetCarDetails();
+            //var result = _carService.GetCarDetails();
+            var result = _carService.GetAll();
 
-            return Ok(result.Data);
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(CarAddDto carDto)
+        {
+            var result = _carService.Add(carDto);
+
+            if (result.IsSuccess)
+                return Ok(result.Message);
+
+            return BadRequest(result.Message);
+
         }
     }
 }
