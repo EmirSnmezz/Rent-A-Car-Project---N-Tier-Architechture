af5fb001e77a80adf3a320c63b075ce338216536
EmirSonmez28 <emir.sonmez28@outlook.com>
2025-10-01 00:46:51 +0300

    service tool and core module integrated for Dependency Injection.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,9 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encyrption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Concrete.Base;
@@ -28,9 +31,16 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
         };
     });
+
+ServiceTool.Create(builder.Services);
+
 builder.Services.AddAuthorization();
 
 builder.Services.AddDbContext<RentalCarDbContext>(ServiceLifetime.Singleton);
+
+builder.Services.AddDependencyResolvers(new Core.Utilities.IoC.ICoreModule[] { 
+    new CoreModule() 
+});
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
