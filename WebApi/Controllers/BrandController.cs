b64d263e858ce60db29428ce45d893eb5ff730a9
emir <emircan_snmez@outlook.com>
2025-12-14 03:33:31 +0300

    Brand and Model controllers created and add and getall operations integrated into API

---
+++ WebApi/Controllers/BrandController.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Base;
+
+namespace WebApi.Controllers
+{
+    public class BrandController : BaseController
+    {
+
+        IBrandService _brandService;
+
+        public BrandController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpPost("Add")]
+
+        public IActionResult AddBrand(Brand brand)
+        {
+            var result = _brandService.Add(brand);
+
+            if(result.IsSuccess)
+            {
+                return Created();
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAllBrand()
+        {
+            var result = _brandService.GetAll();
+            return Ok(result);
+        }
+    }
+}
---
+++ WebApi/Controllers/ModelController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Base;
+
+namespace WebApi.Controllers
+{
+    public class ModelController: BaseController
+    {
+        IModelService _modelService;
+        public ModelController(IModelService modelService) 
+        {
+            _modelService = modelService;
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(Model model)
+        {
+            _modelService.Add(model);
+            return Created();
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _modelService.GetAll();
+
+            return Ok(result);
+        }
+    }
+}
